66263e42430d268b0eede0809d0a13e619896e2c
Rares Ionescu <im.rares03@gmail.com>
2024-01-31 11:37:02 +0000

    Added "checked" property to task items. Through it, we can check which items are crossed out, so they get rendered at the bottom of the list.

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,18 +3,21 @@ import { useState } from "react";
 interface Task {
   id: number;
   text: string;
+  checked: boolean;
 }
 
 interface TaskItemProps {
   task: Task;
   deleteTask: (id: number) => void;
+  toggleTask: (taskId: number) => void;
 }
 
-const TaskItem = ({ task, deleteTask }: TaskItemProps) => {
+const TaskItem = ({ task, deleteTask, toggleTask }: TaskItemProps) => {
   const [checked, setChecked] = useState(false);
 
   const handleCheckboxChange = () => {
     setChecked(!checked);
+    toggleTask(task.id);
   };
 
   return (
