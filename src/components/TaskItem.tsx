1b7e6ef02146f57c69df0ffe968a24647e917cc2
Rares Ionescu <im.rares03@gmail.com>
2024-02-01 15:00:17 +0000

    Added edit, cancel and save buttons and functionalities.

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -10,14 +10,40 @@ interface TaskItemProps {
   task: Task;
   deleteTask: (id: number) => void;
   toggleTask: (taskId: number) => void;
+  editTask: (taskId: number, newText: string) => void;
 }
 
-const TaskItem = ({ task, deleteTask, toggleTask }: TaskItemProps) => {
+const TaskItem = ({
+  task,
+  deleteTask,
+  toggleTask,
+  editTask,
+}: TaskItemProps) => {
   const [checked, setChecked] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(task.text);
 
   const handleCheckboxChange = () => {
     setChecked(!checked);
     toggleTask(task.id);
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    editTask(task.id, editedText);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditedText(task.text);
+    setIsEditing(false);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedText(event.target.value);
   };
 
   return (
@@ -27,14 +53,44 @@ const TaskItem = ({ task, deleteTask, toggleTask }: TaskItemProps) => {
         checked={checked}
         onChange={handleCheckboxChange}
       />
-      <li className="task-text">{task.text}</li>
-      <button
-        type="button"
-        className="task-delete"
-        onClick={() => deleteTask(task.id)}
-      >
-        Delete
-      </button>
+      {isEditing ? (
+        <>
+          <input
+            className="text-edit"
+            type="text"
+            value={editedText}
+            onChange={handleChange}
+          />
+
+          <button type="button" className="task-save" onClick={handleSaveClick}>
+            &#10004; {/*Checkmark*/}
+          </button>
+
+          <button
+            type="button"
+            className="task-cancel"
+            onClick={handleCancelClick}
+          >
+            &#10006; {/*Cross*/}
+          </button>
+        </>
+      ) : (
+        <>
+          <li className="task-text">{task.text}</li>
+
+          <button type="button" className="task-edit" onClick={handleEditClick}>
+            Edit
+          </button>
+
+          <button
+            type="button"
+            className="task-delete"
+            onClick={() => deleteTask(task.id)}
+          >
+            Delete
+          </button>
+        </>
+      )}
     </div>
   );
 };
